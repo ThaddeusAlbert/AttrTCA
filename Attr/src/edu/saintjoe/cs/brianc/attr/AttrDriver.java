120be92b60a3067287513c6f27c7e8e04b14c565
thaddeusalbert <talbert@saintjoe.edu>
2015-04-20 14:19:21 -0400

    Final Commit

---
+++ Attr/src/edu/saintjoe/cs/brianc/attr/AttrDriver.java
@@ -0,0 +1,9 @@
+package edu.saintjoe.cs.brianc.attr;
+
+public class AttrDriver {
+	private Attr one; 
+	private Attr two; 
+	private Attr three;
+ 	public static void main(String[] args){
+ 	one = new Attr("Name', 'Thaddeus");
+}
